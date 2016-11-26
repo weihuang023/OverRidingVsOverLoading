e1b52a7efabf6ec2f9c08cad2ee68f776c4e2629
Wei Huang <weih81888@gmail.com>
2016-11-26 14:07:24 -0500

    Update the Code Overload and System.out.println

--- src/Overload.java
+++ src/Overload.java
@@ -3,15 +3,16 @@ public class Overload {
 
 	
 	static void buy(int itemId, double amount){
-		System.out.println("Id: 100, Amount: 100");
+		System.out.println("ID: 1001, Amount: 100");
 	}
 	static void buy(String name, int numOfItems){
-		System.out.println("ToothBush, 3");
+		System.out.println("Brand: 18F, QTY: 1");
 	}
 	
 	public static void main(String arg[]){
-		buy(1,1.0);
+		buy(1,100);
 		buy("item",3);
+
 		
 		//This is new message.
 	}
